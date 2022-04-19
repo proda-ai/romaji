a7738ea6564a45b9edcb32cc3d33e50120005072
Michał J. Gajda <mjgajda@migamake.com>
2022-04-19 18:39:23 +0200

    Romanizing consonants.

--- romaja/src/Data/Char/Romaja.hs
+++ romaja/src/Data/Char/Romaja.hs
@@ -29,13 +29,51 @@ isKoreanSyllableChar c = ord c >= 0xAC00 && ord c <= 0xD7A3 -- Hangul syllables
 
 decomposeKoreanSyllableChar c = [initialChar, medialChar] <> finalChar 
   where
-    (rest   , final ) = (traceIt "a" (ord c - 44032)) `quotRem`  28
-    (initial, medial) = (traceIt "b" rest           ) `quotRem` (588 `div` 28)
+    (rest   , final ) = (ord c - 44032) `quotRem`            28
+    (initial, medial) = rest            `quotRem` (588 `div` 28)
     initialChar = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ"    !! traceIt "initial" initial
     medialChar  = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ" !! traceIt "medial"  medial
     finalChar   = case traceIt "final" final of
                     0 -> []
                     otherFinal -> [" ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ" !! final]
+
+data ConsonantPos = Initial | Final
+
+romajanizeConsonant :: Char -> ConsonantPos -> [Char]
+romajanizeConsonant 'ㄱ' Initial = "g"
+romajanizeConsonant 'ㄱ' Final   = "k"
+romajanizeConsonant 'ㄲ' Initial = "kk"
+romajanizeConsonant 'ㄲ' Final   = "k"
+romajanizeConsonant 'ㄴ' _       = "k"
+romajanizeConsonant 'ㄷ' Initial = "d"
+romajanizeConsonant 'ㄷ' Final   = "t"
+romajanizeConsonant 'ㄸ' Initial = "tt"
+romajanizeConsonant 'ㄸ' Final   = "" -- TODO: or no character?
+romajanizeConsonant 'ㄹ' Initial = "r"
+romajanizeConsonant 'ㄹ' Final   = "l"
+romajanizeConsonant 'ㅁ' _       = "m"
+romajanizeConsonant 'ㅂ' Initial = "b"
+romajanizeConsonant 'ㅂ' Final   = "p"
+romajanizeConsonant 'ㅃ' Initial = "pp"
+romajanizeConsonant 'ㅃ' Final   = ""
+romajanizeConsonant 'ㅅ' Initial = "s"
+romajanizeConsonant 'ㅅ' Final   = "t"
+romajanizeConsonant 'ㅆ' Initial = "ss"
+romajanizeConsonant 'ㅆ' Final   = "t"
+romajanizeConsonant 'ㅇ' Initial = ""
+romajanizeConsonant 'ㅇ' Final   = "ng"
+romajanizeConsonant 'ㅈ' Initial = "j"
+romajanizeConsonant 'ㅈ' Final   = "t"
+romajanizeConsonant 'ㅉ' Initial = "jj"
+romajanizeConsonant 'ㅉ' Final   = ""
+romajanizeConsonant 'ㅊ' Initial = "ch"
+romajanizeConsonant 'ㅊ' Final   = "t"
+romajanizeConsonant 'ㅋ' _       = "k"
+romajanizeConsonant 'ㅌ' _       = "t"
+romajanizeConsonant 'ㅍ' _       = "p"
+romajanizeConsonant 'ㅎ' _       = "h"
+romajanizeConsonant 'ㅎ' _       = "t"
+romajanizeConsonant  c    _     = [c]
 
 isLatinChar :: Char -> Bool
 isLatinChar = isLatin1
