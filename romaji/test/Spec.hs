fee2e6c60ae4098fa3b2b64074b53b9cf264be28
Michał J. Gajda <mjgajda@migamake.com>
2022-04-25 17:55:26 +0200

    Few more examples.

--- romaji/test/Spec.hs
+++ romaji/test/Spec.hs
@@ -1,20 +1,25 @@
 module Main where
 
 import Test.Hspec
+import Data.Char(toLower)
 import Data.Char.Romaji
 import Data.Text.Normalize
 import qualified Data.Text as T
 
 checkRomajization jp en =
   it (jp <> " is romanized to " <> en) $
-    romajize jp `shouldBe` T.unpack (normalize NFD (T.pack en))
+    romajize jp `shouldBe` T.unpack (normalize NFD $ T.pack $ fmap toLower en)
 
 main :: IO ()
 main = hspec $ do
   describe "Wikipedia examples" $ do
-    checkRomajization "ローマじ" "rômazi"
-    checkRomajization "ふじさん" "Huzisan"
-    checkRomajization "おちゃ"   "otya"
-    checkRomajization "ちじ"     "tizi"
-    checkRomajization "ちぢむ"   "tizimu"
-    checkRomajization "つづく"   "tuzuku"
+    describe "hiragana" $ do 
+        checkRomajization "ローマじ" "rômazi"
+        checkRomajization "ふじさん" "Huzisan"
+        checkRomajization "おちゃ"   "otya"
+        checkRomajization "ちじ"     "tizi"
+        checkRomajization "ちぢむ"   "tizimu"
+        checkRomajization "つづく"   "tuzuku"
+        checkRomajization "非ヘボン式ローマ字"   "hi-Hebon-shiki rōmaji"
+    describe "Mixed Hiragana and Kanji" $ do 
+        checkRomajization "結婚する"  "kekkonsuru"
