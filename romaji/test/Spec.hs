50c6add0291681afb2cbf70c0131cb4da543bf02
Michał J. Gajda <mjgajda@migamake.com>
2022-04-25 17:46:57 +0200

    First attempt at Japanese romanization.

--- romaji/test/Spec.hs
+++ romaji/test/Spec.hs
@@ -1,2 +1,20 @@
+module Main where
+
+import Test.Hspec
+import Data.Char.Romaji
+import Data.Text.Normalize
+import qualified Data.Text as T
+
+checkRomajization jp en =
+  it (jp <> " is romanized to " <> en) $
+    romajize jp `shouldBe` T.unpack (normalize NFD (T.pack en))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Wikipedia examples" $ do
+    checkRomajization "ローマじ" "rômazi"
+    checkRomajization "ふじさん" "Huzisan"
+    checkRomajization "おちゃ"   "otya"
+    checkRomajization "ちじ"     "tizi"
+    checkRomajization "ちぢむ"   "tizimu"
+    checkRomajization "つづく"   "tuzuku"
